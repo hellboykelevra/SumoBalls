c5a918755a2801c6b7e281ce16044b44fc214d32
Dorian <248930693+Rookie-Studios@users.noreply.github.com>
2026-01-19 16:57:11 +0000

    Acumulación energía End

--- Assets/Scripts/PorcentajeController.cs
+++ Assets/Scripts/PorcentajeController.cs
@@ -3,24 +3,52 @@ using UnityEngine;
 
 public class PorcentajeController : MonoBehaviour
 {
-    public float fuerza = 1.5f;
-    
+    public float energy = 0;
+    Rigidbody2D meRG;
+    private void Start()
+    {
+        meRG = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        Vector2 direcciton = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (this.CompareTag("BallR")) 
+            {
+                meRG.AddForce(direcciton * energy, ForceMode2D.Impulse);
+
+            }
+                    
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            if (this.CompareTag("BallB"))
+            {
+                meRG.AddForce(direcciton * energy, ForceMode2D.Impulse);
+            }
+                
+
+        }
+
+    }
     // Método para detectar la colisión entre las dos bolas y darle fuerza
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Choque detectado entre las dos bolas");
 
-        if (collision.gameObject.CompareTag("Bola")) 
+        if (collision.gameObject.CompareTag("BallR")|| collision.gameObject.CompareTag("BallB")) 
         {
-            Rigidbody2D rbBlueplayer = GetComponent<Rigidbody2D>();
-            Rigidbody2D rbRedplayer = collision.gameObject.GetComponent<Rigidbody2D>();
-
+            energy +=1.5f;
+            Rigidbody2D otherRG = collision.gameObject.GetComponent<Rigidbody2D>();
             //Dirección de las dos bolas
-            Vector2 direccion = (transform.position - collision.transform.position).normalized;
+            Vector2 direccionOther = (transform.position - collision.transform.position).normalized;
 
 
             // Aplicar fuerzas a las dos bolas
-            rbBlueplayer.AddForce(-direccion * fuerza, ForceMode2D.Impulse);
+            meRG.AddForce(direccionOther * energy, ForceMode2D.Impulse);
         }
         
     }
